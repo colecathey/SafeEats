35840875297a5a12c439be2feabc14af04ac45c2
colecathey <cole.cathey@outlook.com>
2015-11-29 21:07:02 -0600

    added ingredient tests

---
+++ SafeEats/SafeEats.Tests/models/IngredientTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SafeEats.Models;
+
+namespace SafeEats.Tests.models
+{
+    /// <summary>
+    /// Summary description for IngredientTests
+    /// </summary>
+    [TestClass]
+    public class IngredientTests
+    {
+        [TestMethod]
+        public void CardEnsureICanCreateInstance()
+        {
+            Ingredient ingredient = new Ingredient();
+            Assert.IsNotNull(ingredient);
+        }
+
+        [TestMethod]
+        public void CardEnsurePropertiesWork()
+        {
+            
+            // Object Initializer syntax
+            Ingredient ingredient = new Ingredient { IngredientName = "Carrot", IngredientId = 4, /*need to input type*/ };
+            
+            Assert.AreEqual("Carrot", ingredient.IngredientName);
+            Assert.AreEqual(4, ingredient.IngredientId);
+            //Assert.AreEqual(???, ingredient.IngredientType);
+        }
+        
+    }
+}

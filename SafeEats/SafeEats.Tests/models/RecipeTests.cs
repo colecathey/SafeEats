22a7e9120d8530a12d604673cc56ee78bd144c4a
colecathey <cole.cathey@outlook.com>
2015-11-29 21:16:55 -0600

    added test parameters

--- SafeEats/SafeEats.Tests/models/RecipeTests.cs
+++ SafeEats/SafeEats.Tests/models/RecipeTests.cs
@@ -1,14 +1,35 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SafeEats.Models;
 
 namespace SafeEats.Tests.models
 {
     [TestClass]
-    public class UnitTest1
+    public class RecipeTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CardEnsureICanCreateRecipeInstance()
         {
+            Recipe recipe = new Recipe();
+            Assert.IsNotNull(recipe);
+        }
+
+        [TestMethod]
+        public void CardEnsurePropertiesWork()
+        {
+            var tom = new ApplicationUser();
+            
+
+            // Object Initializer syntax
+            Recipe recipe = new Recipe { RecipeName = "Soup", RecipeId = 4, RecipeCreator = tom,
+                RecipeDescription = "recipe", /*need to add ingredient list here*/ RecipeRecipe = "stir", RecipeType = true };
+
+            Assert.AreEqual("Soup", recipe.RecipeName);
+            Assert.AreEqual(4, recipe.RecipeId);
+            Assert.AreEqual(tom, recipe.RecipeCreator);
+            Assert.AreEqual("stir", recipe.RecipeRecipe);
+            Assert.AreEqual(true, recipe.RecipeType);
+            //Assert.AreEqual(???, recipe.RecipeIngredients);
         }
     }
 }

197cd31c1d546cb7b6e07924e51cd9ae82aede5f
colecathey <cole.cathey@outlook.com>
2015-11-30 17:52:20 -0600

    messing with recipes

--- SafeEats/SafeEats/Models/RecipeRepository.cs
+++ SafeEats/SafeEats/Models/RecipeRepository.cs
@@ -7,5 +7,38 @@ namespace SafeEats.Models
 {
     public class RecipeRepository
     {
+        private RecipeContext context;
+
+        public RecipeRepository()
+        {
+            context = new RecipeContext();
+        }
+        public RecipeRepository(RecipeContext _context)
+        {
+            context = _context;
+        }
+
+        // void or bool or Recipe
+        public bool AddRecipe(int _recipe_id, Recipe _list)
+        {
+            var query = from r in context.Recipes where r.RecipeId == _recipe_id select r;
+            Recipe found_recipe = null;
+            bool result = true;
+            try
+            {
+                found_recipe = query.Single<Recipe>();
+                found_recipe.Recipe.Add(_list);
+                context.SaveChanges();
+            }
+            catch (InvalidOperationException)
+            {
+                result = false;
+            }
+            catch (ArgumentNullException)
+            {
+                result = false;
+            }
+            return result;
+        }
     }
 }

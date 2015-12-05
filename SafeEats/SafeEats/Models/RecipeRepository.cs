63bc0c4c54082fdf792c6c82007be527957cc42d
colecathey <cole.cathey@outlook.com>
2015-12-04 21:28:17 -0600

    added GetListCount()

--- SafeEats/SafeEats/Models/RecipeRepository.cs
+++ SafeEats/SafeEats/Models/RecipeRepository.cs
@@ -66,7 +66,7 @@ namespace SafeEats.Models
 
         public int GetListCount()
         {
-            throw new NotImplementedException();
+            return GetAllLists().Count;
         }
 
         public object GetAllLists()

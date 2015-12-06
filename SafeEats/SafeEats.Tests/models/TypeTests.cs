827da8a49830c5b977cadac963fcec1be9aa3314
colecathey <cole.cathey@outlook.com>
2015-12-05 23:12:41 -0600

    added instantiation tests

--- SafeEats/SafeEats.Tests/models/TypeTests.cs
+++ SafeEats/SafeEats.Tests/models/TypeTests.cs
@@ -1,14 +1,29 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SafeEats.Models;
 
 namespace SafeEats.Tests.models
 {
     [TestClass]
-    public class UnitTest1
+    public class TypeTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CardEnsureICanCreateInstance()
         {
+            Type recipeType = new Type();
+            Assert.IsNotNull(recipeType);
+        }
+
+        [TestMethod]
+        public void CardEnsurePropertiesWork()
+        {
+
+            // Object Initializer syntax
+            Type recipeType = new Models.Type { TypeName = "Veg", TypeId = 4, TypeType = 1 };
+
+            Assert.AreEqual("Veg", recipeType.TypeName);
+            Assert.AreEqual(4, recipeType.TypeId);
+            Assert.AreEqual(1, recipeType.TypeType);
         }
     }
 }
